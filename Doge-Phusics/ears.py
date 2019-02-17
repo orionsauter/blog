0d6387777d1a9e071760021b945709801d02e3fd
Orion Sauter <orionsauter@gmail.com>
2019-02-17 14:09:21 +0100

    Switch to HTML5, and cycle through options

--- Doge-Phusics/ears.py
+++ Doge-Phusics/ears.py
@@ -22,66 +22,72 @@ amp = 15 # Stride amplitude
 mdist = 'quad' # Mass distribution
 fig, ax = plt.subplots()
 
-# Setup
-th = np.zeros(N)
-th[0] = th0
-dth = np.zeros(N)
-if (mdist == 'unif'):
-    dist = np.ones(N)
-elif (mdist == 'lin'):
-    dist = np.arange(N+6, 6, -1)
-elif (mdist == 'quad'):
-    dist = np.arange(N+10, 10, -1)**2
-dm = m*dist/np.sum(dist)
-dL = L/N
-dI = dm*dL**2/3
-ears, = plt.plot([],[],'-')
+for k in (200, 400):
+    for mdist in ('unif', 'lin', 'quad'):
+        # Setup
+        fig.clear()
+        th = np.zeros(N)
+        th[0] = th0
+        dth = np.zeros(N)
+        if (mdist == 'unif'):
+            dist = np.ones(N)
+        elif (mdist == 'lin'):
+            dist = np.arange(N+6, 6, -1)
+        elif (mdist == 'quad'):
+            dist = np.arange(N+10, 10, -1)**2
+        dm = m*dist/np.sum(dist)
+        dL = L/N
+        dI = dm*dL**2/3
+        ears, = plt.plot([],[],'-')
 
-# Transform angles to (x,y)
-def th2xy(th):
-    thtot = np.cumsum(th, axis=0)
-    dxy = np.zeros((N, 2))
-    for i in range(N):
-        dxy[i,:] = [dL*np.cos(thtot[i]), dL*np.sin(thtot[i])]
-    dxy = np.insert(dxy, 0, [0, 0], axis=0)
-    xy = np.cumsum(dxy, axis=0)
-    return xy
+        # Transform angles to (x,y)
+        def th2xy(th):
+            thtot = np.cumsum(th, axis=0)
+            dxy = np.zeros((N, 2))
+            for i in range(N):
+                dxy[i,:] = [dL*np.cos(thtot[i]), dL*np.sin(thtot[i])]
+            dxy = np.insert(dxy, 0, [0, 0], axis=0)
+            xy = np.cumsum(dxy, axis=0)
+            return xy
 
-# Plot segments
-def draw(th):
-    fig.clear()
-    xy = th2xy(th)
-    ears, = plt.plot(xy[:,0],xy[:,1],'.-')
-    plt.xlim([-0.1, 1.0*L])
-    plt.ylim([-0.1, 1.0*L])
-    return ears
+        # Plot segments
+        def draw(th):
+            fig.clear()
+            xy = th2xy(th)
+            ears, = plt.plot(xy[:,0],xy[:,1],'.-')
+            plt.xlim([-0.1, 1.0*L])
+            plt.ylim([-0.1, 1.0*L])
+            return ears
 
-# Calculate torque
-def torque(frame, th, dth):
-    trq = np.zeros(N)
-    thtot = np.cumsum(th, axis=0)
-    for i in range(N):
-        theta = th[i]
-        if i == 0:
-            theta -= th0
-        trq[i] = -k*theta - dI[i]/4*g*np.cos(thtot[i]) - mu*dth[i]
-    trq[0] += amp*np.sin(freq*frame)
-    return trq
+        # Calculate torque
+        def torque(frame, th, dth):
+            trq = np.zeros(N)
+            thtot = np.cumsum(th, axis=0)
+            for i in range(N):
+                theta = th[i]
+                if i == 0:
+                    theta -= th0
+                trq[i] = -k*theta - dI[i]/4*g*np.cos(thtot[i]) - mu*dth[i]
+            trq[0] += amp*np.sin(freq*frame)
+            return trq
 
-# Init function for animation
-def init():
-    plt.xlim([-0.1, 1.0*L])
-    plt.ylim([-0.1, 1.0*L])
-    return ears,
+        # Init function for animation
+        def init():
+            plt.xlim([-0.1, 1.0*L])
+            plt.ylim([-0.1, 1.0*L])
+            return ears,
 
-# Perform integration
-def update(frame, th, dth, ears):
-    th += dth*dt
-    dth += torque(frame, th, dth)*dt/dI
-    xy = th2xy(th)
-    ears.set_data(xy[:,0], xy[:,1])
-    return ears,
+        # Perform integration
+        def update(frame, th, dth, ears):
+            th += dth*dt
+            dth += torque(frame, th, dth)*dt/dI
+            xy = th2xy(th)
+            ears.set_data(xy[:,0], xy[:,1])
+            return ears,
 
-ani = animation.FuncAnimation(fig, update, frames=np.arange(0, 10, 0.01),
-                              init_func=init, fargs=[th, dth, ears], blit=True)
-ani.save('ears-'+mdist+'-'+str(k)+'.mp4', dpi=100, writer=writer)
+        ani = animation.FuncAnimation(fig, update, frames=np.arange(0, 10, 0.01),
+                                      init_func=init, fargs=[th, dth, ears],
+                                      blit=True, interval=dt*1e3)
+        f = open('ears-'+mdist+'-'+str(k)+'.html', 'w')
+        f.write(ani.to_html5_video())
+        f.close()
